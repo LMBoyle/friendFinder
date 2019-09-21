44fa15f4c728fd5ee9c714ad96449fee61b11fa2
Luke Boyle <boylelukem@gmail.com>
2019-09-21 16:32:06 -0400

    added basic express set-up

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
+// Dependencies =============================================================
+
 var express = require("express");
 var path = require("path");
+
+// Express ==================================================================
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes ===================================================================
+
+// Listen ===================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
