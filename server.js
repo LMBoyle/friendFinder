2523722931a1b704bc0cd0e07471780021ca1a47
Luke Boyle <boylelukem@gmail.com>
2019-09-21 20:55:55 -0400

    added routes to home, survey and if anything else is typed in

--- server.js
+++ server.js
@@ -6,12 +6,30 @@ var path = require("path");
 // Express ==================================================================
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 1745;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes ===================================================================
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+// API Routes
+app.get("/api/", function(req, res) {
+  return res.json(characters);
+});
+
+// All Other Routes
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/home.html"))
+});
 
 // Listen ===================================================================
 
