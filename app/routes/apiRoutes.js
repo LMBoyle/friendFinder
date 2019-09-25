716a90a6aa1565b9fb7e90f49c35406a87e1c64b
Luke Boyle <boylelukem@gmail.com>
2019-09-24 20:48:19 -0400

    started function to find friend match

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -12,9 +12,33 @@ module.exports = function(app) {
   });
 
   // API POST ===============================================================
-  // Add a new friend
+  // Add a new friend and find match
   app.post("/api/friends", function(req, res) {
+    // Current User
+    var user = req.body;
+    // Start with first friend
+    var friendIndex = 0;
+    // Start with the max difference
+    var maxDifference = 40;
+
+    // TODO Go through each person and compare scores
+    for (var f = 0; f < friendData.length; f++) {
+      var currDiff = 0;
+      // TODO For each friend compare scores
+      for (var s = 0; s < friendData[f].scores.length; s++) {
+        currDiff = Math.abs(user.scores[s] - friendData[f].scores[s])
+      }
+
+      // If current difference is less than the max difference, update the friend
+      if (currDiff < maxDifference) {
+        friendIndex = f;
+        maxDifference = currDiff;
+      }
+    }
+
     // Push users data
-    friendData.push(req.body)
+    friendData.push(req.body);
+    // Send match back to browser
+    res.json(friendData[friendIndex])
   })
 }
